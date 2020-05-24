ce8e2574b401f42f2a06aef253228069128df9a8
rparpj016 <rparpj016@outlook.com>
2020-05-24 22:43:16 +0100

    Adding VM cluster and Hostname PS1

---
+++ VMs_clustername.ps1
@@ -0,0 +1,17 @@
+﻿###VM count by clustername. ###
+
+#Synopsis 
+#Date : 30/03/2020
+#Author : Rafeekh
+#Modified by : Rafeekh
+# This script will list the VM counts by cluster name.
+
+#Connect to the Virtual centre uses passord from encrypted file
+Connect-VIServer -Server 10.125.1.10 -Credential (Import-Clixml -Path D:\Scripts\VMcount\pass\AMS\cred.xml)
+
+Get-Cluster | Select Name, @{N="Host Count"; E={($_ | Get-VMHost).Count}},  @{N="VM Count"; E={($_ | Get-VM).Count}} | export-csv "D:\Scripts\VMcount\vc_capacity_$vc.csv" 
+#Disconnect from Virtual centre
+Disconnect-VIServer -Server * -Force -Confirm:$false
+
+
+### End of Script###
---
+++ VMs_hostname.ps1
@@ -0,0 +1,17 @@
+﻿###VM count by hostname. ###
+
+#Synopsis 
+#Date : 30/03/2020
+#Author : Rafeekh
+#Modified by : Rafeekh
+# This script will list the VM counts by host name.
+
+#Connect to the Virtual centre uses passord from encrypted file
+Connect-VIServer -Server 10.125.1.10 -Credential (Import-Clixml -Path D:\Scripts\VMcount\pass\AMS\cred.xml)
+Get-VMHost | Select @{N=“Cluster“;E={Get-Cluster -VMHost $_}}, Name, @{N=“NumVM“;E={($_ | Get-VM).Count}} | Sort Cluster, Name | Export-Csv -NoTypeInformation D:\Scripts\VMcount\clu-host-numvm.csv 
+
+#Disconnect from Virtual centre
+Disconnect-VIServer -Server * -Force -Confirm:$false
+
+
+### End of Script###
